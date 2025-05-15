944096158e12f00722e4f16f8044a5cf5f0e037a
sabbirHossain <sabbirshnt@gmail.com>
2025-05-15 15:37:16 +0600

    done

--- src/components/modules/Deposit/BankAccount.jsx
+++ src/components/modules/Deposit/BankAccount.jsx
@@ -16,7 +16,7 @@ import PG from "./PaymentMethod/PG";
 
 const BankAccount = ({ amount }) => {
   const paymentMethodRef = useRef();
-  const [getPaymentMethod, { data }] = useBankAccountMutation();
+  const [getPaymentMethod, { data, isSuccess }] = useBankAccountMutation();
   const [methodType, setMethodType] = useState(null);
   const [depositData, setDepositData] = useState({});
   const [paymentId, setPaymentId] = useState(null);
@@ -84,71 +84,74 @@ const BankAccount = ({ amount }) => {
                 your main wallet<span>.</span>
               </p>
             </section>
-            <div className="rounded-lg bg-bg_color_primary overflow-hidden text-text_color_primary1 transition-height duration-500 ease-in-out h-max px-3 pt-[15px] pb-[20px]">
-              <div className="flex flex-row justify-end items-center w-full">
-                <span className="text-base font-lato font-bold leading-5 w-full">
-                  Payment Options
-                  <span className="text-text_color_success font-normal leading-4">
-                    ({data?.result?.length})
-                  </span>
-                </span>
-                <button
-                  onClick={() => scrollToLeft(paymentMethodRef)}
-                  className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out flex size-[22px] sm:size-[24px] md:size-[26px] justify-center items-center gap-[10px] bg-bg_color_quaternary rounded shadow-sm cursor-pointer"
-                  type="button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="var(--bg-active-primary)"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M15 6l-6 6l6 6" />
-                  </svg>
-                </button>
-                <button
-                  onClick={() => scrollToRight(paymentMethodRef)}
-                  className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out flex size-[22px] sm:size-[24px] md:size-[26px] justify-center ml-[4px] items-center gap-[10px] bg-bg_color_quaternary rounded shadow-sm cursor-pointer"
-                  type="button"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={20}
-                    height={20}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="var(--bg-active-primary)"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M9 6l6 6l-6 6" />
-                  </svg>
-                </button>
-              </div>
-              <div
-                ref={paymentMethodRef}
-                id="payMentOptions"
-                className="flex items-center gap-x-1.5 pt-[18px] pb-[8px] overflow-x-auto no-scrollbar scroll-smooth cursor-pointer w-full transition-all ease-in-out duration-150"
-              >
-                {data?.result?.map((method) => (
+
+            {data?.result?.length > 0 && (
+              <>
+                <div className="rounded-lg bg-bg_color_primary overflow-hidden text-text_color_primary1 transition-height duration-500 ease-in-out h-max px-3 pt-[15px] pb-[20px]">
+                  <div className="flex flex-row justify-end items-center w-full">
+                    <span className="text-base font-lato font-bold leading-5 w-full">
+                      Payment Options
+                      <span className="text-text_color_success font-normal leading-4">
+                        ({data?.result?.length})
+                      </span>
+                    </span>
+                    <button
+                      onClick={() => scrollToLeft(paymentMethodRef)}
+                      className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out flex size-[22px] sm:size-[24px] md:size-[26px] justify-center items-center gap-[10px] bg-bg_color_quaternary rounded shadow-sm cursor-pointer"
+                      type="button"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="var(--bg-active-primary)"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M15 6l-6 6l6 6" />
+                      </svg>
+                    </button>
+                    <button
+                      onClick={() => scrollToRight(paymentMethodRef)}
+                      className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out flex size-[22px] sm:size-[24px] md:size-[26px] justify-center ml-[4px] items-center gap-[10px] bg-bg_color_quaternary rounded shadow-sm cursor-pointer"
+                      type="button"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="var(--bg-active-primary)"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 6l6 6l-6 6" />
+                      </svg>
+                    </button>
+                  </div>
                   <div
-                    key={method?.paymentId}
-                    onClick={() => handleVisibleBankMethod(method)}
-                    className={`flex justify-start items-center flex-col gap-y-2 rounded-[10px] bg-transparent py-2 w-full px-[20px] relative border  shadow-depositGateWayBoxShadows min-w-[117px] max-w-[130px] ${
-                      method?.type === methodType
-                        ? "border-[var(--bg-active-primary)]"
-                        : ""
-                    }`}
+                    ref={paymentMethodRef}
+                    id="payMentOptions"
+                    className="flex items-center gap-x-1.5 pt-[18px] pb-[8px] overflow-x-auto no-scrollbar scroll-smooth cursor-pointer w-full transition-all ease-in-out duration-150"
                   >
-                    {/* <div className="absolute top-0 right-0.5">
+                    {data?.result?.map((method) => (
+                      <div
+                        key={method?.paymentId}
+                        onClick={() => handleVisibleBankMethod(method)}
+                        className={`flex justify-start items-center flex-col gap-y-2 rounded-[10px] bg-transparent py-2 w-full px-[20px] relative border  shadow-depositGateWayBoxShadows min-w-[117px] max-w-[130px] ${
+                          method?.type === methodType
+                            ? "border-[var(--bg-active-primary)]"
+                            : ""
+                        }`}
+                      >
+                        {/* <div className="absolute top-0 right-0.5">
                       <div className="inline-flex items-center">
                         <label
                           className="relative flex cursor-pointer items-center rounded-full"
@@ -179,58 +182,66 @@ const BankAccount = ({ amount }) => {
                         </label>
                       </div>
                     </div> */}
-                    <div className="flex items-center gap-y-1 flex-col w-full">
-                      {method?.type == "qr" && (
-                        <FaQrcode size={20} color="gray" />
-                      )}
-                      {method?.type == "bank" && (
-                        <CiBank size={20} color="gray" />
-                      )}
-                      {method?.type == "upi" || method?.type == "pg" ? (
-                        <img
-                          style={{ height: "20px", width: "20px" }}
-                          src={"/icon/upi.png"}
-                        />
-                      ) : null}
-                      {method?.type == "usdt" ? (
-                        <img
-                          style={{ height: "20px", width: "20px" }}
-                          src={"/icon/usdt.png"}
-                        />
-                      ) : null}
-                      {method?.type == "whatsapp" ? (
-                        <img
-                          style={{ height: "20px", width: "20px" }}
-                          src={images.whatsapp}
-                        />
-                      ) : null}
+                        <div className="flex items-center gap-y-1 flex-col w-full">
+                          {method?.type == "qr" && (
+                            <FaQrcode size={20} color="gray" />
+                          )}
+                          {method?.type == "bank" && (
+                            <CiBank size={20} color="gray" />
+                          )}
+                          {method?.type == "upi" || method?.type == "pg" ? (
+                            <img
+                              style={{ height: "20px", width: "20px" }}
+                              src={"/icon/upi.png"}
+                            />
+                          ) : null}
+                          {method?.type == "usdt" ? (
+                            <img
+                              style={{ height: "20px", width: "20px" }}
+                              src={"/icon/usdt.png"}
+                            />
+                          ) : null}
+                          {method?.type == "whatsapp" ? (
+                            <img
+                              style={{ height: "20px", width: "20px" }}
+                              src={images.whatsapp}
+                            />
+                          ) : null}
 
-                      <span className="text-xs font-lato font-medium truncate w-full uppercase">
-                        {method?.titles}
-                      </span>
-                    </div>
+                          <span className="text-xs font-lato font-medium truncate w-full uppercase">
+                            {method?.titles}
+                          </span>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                  <p className="text-xs md:text-sm pt-2 font-lato font-normal leading-4">
+                    1. Deposit money only in the below available accounts to get
+                    the fastest credits and avoid possible delays.
+                  </p>
+                  <p className="text-text_color_tertiary1 pt-1 font-sans text-xs md:text-sm font-normal cursor-pointer">
+                    See More..
+                  </p>
+                </div>
+                {/* Payment Method */}
+                {methodType === "bank" && (
+                  <Bank amount={amount} depositData={depositData} />
+                )}
+                {methodType === "upi" && (
+                  <UPI amount={amount} depositData={depositData} />
+                )}
+                {methodType === "qr" && <QR depositData={depositData} />}
+                {methodType === "usdt" && <USDT depositData={depositData} />}
+                {methodType === "pg" && <PG depositData={depositData} />}
+                <UploadTransaction paymentId={paymentId} amount={amount} />
+              </>
+            )}
+
+            {data?.result?.length === 0 && isSuccess && (
+              <div className="w-full flex items-center justify-center text-white lg:mt-20">
+                No payment method available right now.
               </div>
-              <p className="text-xs md:text-sm pt-2 font-lato font-normal leading-4">
-                1. Deposit money only in the below available accounts to get the
-                fastest credits and avoid possible delays.
-              </p>
-              <p className="text-text_color_tertiary1 pt-1 font-sans text-xs md:text-sm font-normal cursor-pointer">
-                See More..
-              </p>
-            </div>
-            {/* Payment Method */}
-            {methodType === "bank" && (
-              <Bank amount={amount} depositData={depositData} />
             )}
-            {methodType === "upi" && (
-              <UPI amount={amount} depositData={depositData} />
-            )}
-            {methodType === "qr" && <QR depositData={depositData} />}
-            {methodType === "usdt" && <USDT depositData={depositData} />}
-            {methodType === "pg" && <PG depositData={depositData} />}
-            <UploadTransaction paymentId={paymentId} amount={amount} />
           </form>
         </div>
       </div>
