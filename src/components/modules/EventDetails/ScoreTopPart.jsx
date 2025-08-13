327529ff2c04345689f848bbea124de2ea2d4ea2
sabbirHossain <sabbirshnt@gmail.com>
2025-08-13 11:30:25 +0600

    done

--- src/components/modules/EventDetails/ScoreTopPart.jsx
+++ src/components/modules/EventDetails/ScoreTopPart.jsx
@@ -48,79 +48,87 @@ const ScoreTopPart = ({ iscore, isMobile }) => {
             </span> */}
         </div>
       </div>
-      <div className=" px-[11px] relative bg-bg_color_primary w-full shadow-md">
-        <div className=" w-full flex items-center py-1 justify-start overflow-scroll no-scrollbar gap-x-2">
-          <div
-            title="Current Over"
-            className=" flex py-1 items-center justify-start gap-x-[7px]"
-          >
-            <span className=" w-max min-w-11 text-xs font-medium text-text_color_primary">
-              Current Over
-            </span>
-            <div className=" flex items-center justify-start gap-x-[11px]">
-              <div className="flex items-center justify-start gap-x-[11px]">
-                {iscore?.currentOver?.map((cv, i) => (
-                  <span
-                    key={i}
-                    className="text-xs font-medium min-w-5 min-h-5 aspect-square flex items-center justify-center shadow-md rounded-full p-1 bg-bg_color_cr_default text-text_color_cr_default border-border_color_cr_default "
-                  >
-                    <span>{cv}</span>
-                  </span>
-                ))}
+      {iscore?.currentOver?.length > 0 || iscore?.previousOver?.length > 0 ? (
+        <div className=" px-[11px] relative bg-bg_color_primary w-full shadow-md">
+          <div className=" w-full flex items-center py-1 justify-start overflow-scroll no-scrollbar gap-x-2">
+            {iscore?.currentOver?.length > 0 && (
+              <div
+                title="Current Over"
+                className=" flex py-1 items-center justify-start gap-x-[7px]"
+              >
+                <span className=" w-max min-w-11 text-xs font-medium text-text_color_primary">
+                  Current Over
+                </span>
+                <div className=" flex items-center justify-start gap-x-[11px]">
+                  <div className="flex items-center justify-start gap-x-[11px]">
+                    {iscore?.currentOver?.map((cv, i) => (
+                      <span
+                        key={i}
+                        className="text-xs font-medium min-w-5 min-h-5 aspect-square flex items-center justify-center shadow-md rounded-full p-1 bg-bg_color_cr_default text-text_color_cr_default border-border_color_cr_default "
+                      >
+                        <span>{cv}</span>
+                      </span>
+                    ))}
+                  </div>
+                </div>
               </div>
+            )}
+
+            <div className=" flex items-center justify-center text-xs gap-x-0.5 font-medium leading-4 border-r border-border_color_primary pr-3 text-text_color_primary">
+              <span>=</span>
+              <span>{iscore?.totalThisOver}</span>
+            </div>
+            {iscore?.previousOver?.length > 0 && (
+              <div
+                title="Last Over"
+                className=" py-1 flex items-center justify-start gap-x-[7px]"
+              >
+                <span className=" w-max min-w-11 text-xs font-medium text-text_color_primary">
+                  Previous Over
+                </span>
+                <div className=" flex items-center justify-start gap-x-[11px]">
+                  <div className="flex items-center justify-start gap-x-[11px]">
+                    {iscore?.previousOver?.map((pv, i) => (
+                      <span
+                        key={i}
+                        className="text-xs font-medium min-w-5 min-h-5 aspect-square flex items-center justify-center shadow-md rounded-full p-1 bg-bg_color_cr_wd text-text_color_cr_wd border-border_color_cr_wd "
+                      >
+                        <span>{pv}</span>
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div className=" flex items-center justify-center text-xs gap-x-0.5 font-medium leading-4 border-r border-border_color_primary pr-5 text-text_color_primary">
+              <span>=</span>
+              <span>{iscore?.totalLastOver}</span>
             </div>
           </div>
-          <div className=" flex items-center justify-center text-xs gap-x-0.5 font-medium leading-4 border-r border-border_color_primary pr-3 text-text_color_primary">
-            <span>=</span>
-            <span>{iscore?.totalThisOver}</span>
-          </div>
-          <div
-            title="Last Over"
-            className=" py-1 flex items-center justify-start gap-x-[7px]"
-          >
-            <span className=" w-max min-w-11 text-xs font-medium text-text_color_primary">
-              Previous Over
-            </span>
-            <div className=" flex items-center justify-start gap-x-[11px]">
-              <div className="flex items-center justify-start gap-x-[11px]">
-                {iscore?.previousOver?.map((pv, i) => (
-                  <span
-                    key={i}
-                    className="text-xs font-medium min-w-5 min-h-5 aspect-square flex items-center justify-center shadow-md rounded-full p-1 bg-bg_color_cr_wd text-text_color_cr_wd border-border_color_cr_wd "
-                  >
-                    <span>{pv}</span>
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className=" flex items-center justify-center text-xs gap-x-0.5 font-medium leading-4 border-r border-border_color_primary pr-5 text-text_color_primary">
-            <span>=</span>
-            <span>{iscore?.totalLastOver}</span>
-          </div>
-        </div>
-        <div className=" absolute z-1 top-1/2 -translate-y-1/2 right-2">
-          <button
-            className="inline-block  leading-normal relative overflow-hidden  transition duration-150 ease-in-out  min-w-5 min-h-5 flex items-center justify-center rounded-[4px] bg-bg_color_quaternary 
+          <div className=" absolute z-1 top-1/2 -translate-y-1/2 right-2">
+            <button
+              className="inline-block  leading-normal relative overflow-hidden  transition duration-150 ease-in-out  min-w-5 min-h-5 flex items-center justify-center rounded-[4px] bg-bg_color_quaternary 
       cursor-pointer
       "
-            type="button"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              viewBox="0 0 20 20"
-              fill="none"
+              type="button"
             >
-              <path
-                d="M10.4998 10L6.6665 6.16667L7.83317 5L12.8332 10L7.83317 15L6.6665 13.8333L10.4998 10Z"
-                fill="var(--bg-active-primary)"
-              />
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={20}
+                height={20}
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <path
+                  d="M10.4998 10L6.6665 6.16667L7.83317 5L12.8332 10L7.83317 15L6.6665 13.8333L10.4998 10Z"
+                  fill="var(--bg-active-primary)"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
