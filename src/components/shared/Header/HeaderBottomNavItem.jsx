1da4e392d95119f54d940a0463dde9af3b505628
sabbirHossain <sabbirshnt@gmail.com>
2025-11-01 10:52:27 +0600

    done

--- src/components/shared/Header/HeaderBottomNavItem.jsx
+++ src/components/shared/Header/HeaderBottomNavItem.jsx
@@ -118,7 +118,7 @@ const HeaderBottomNavItem = () => {
             Bihar Election
           </span>
         </Link>
-        <Link
+        {/* <Link
           title="Home"
           className={`cursor-pointer uppercase min-h-[28px]  min-w-[100px] px-[9px]  flex items-center justify-center gap-x-1 py-1 rounded-md text-sm
         ${
@@ -205,7 +205,7 @@ const HeaderBottomNavItem = () => {
             </svg>
           </span>
           <span className="font font-lato text-[12px]  font-semibold">PKL</span>
-        </Link>
+        </Link> */}
         <Link
           onClick={() => handleSetGroup(4)}
           title="Cricket"
