148f2abdc52c4c51f80e3f28bf545ac2730c1dc7
sabbirHossain <sabbirshnt@gmail.com>
2026-01-10 18:47:50 +0600

    done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -31,7 +31,7 @@ const MainLayout = () => {
     const newVersion = socialLink?.build_version;
     if (!stored_build_version) {
       if (newVersion) {
-        setShowBuildVersion(true);
+        localStorage.setItem("build_version", newVersion);
       }
     }
     if (stored_build_version && newVersion) {
