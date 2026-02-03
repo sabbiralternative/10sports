a3894358c5385b88e0e9154e96c41a1552cf9f57
sabbirHossain <sabbirshnt@gmail.com>
2026-02-03 14:05:21 +0600

    done

--- src/components/shared/Header/HeaderBottomNavItem.jsx
+++ src/components/shared/Header/HeaderBottomNavItem.jsx
@@ -77,7 +77,7 @@ const HeaderBottomNavItem = () => {
             {languageValue(valueByLanguage, LanguageKey.HOME)}
           </span>
         </Link>
-        <Link
+        {/* <Link
           title="Home"
           className={`cursor-pointer uppercase min-h-[28px]  min-w-[100px] px-[9px]  flex items-center justify-center gap-x-1 py-1 rounded-md text-sm
         ${
@@ -120,7 +120,7 @@ const HeaderBottomNavItem = () => {
             </svg>
           </span>
           <span className="font font-lato text-[12px]  font-semibold">BBL</span>
-        </Link>
+        </Link> */}
         {socialLink?.referral && (
           <a
             onClick={() => {
